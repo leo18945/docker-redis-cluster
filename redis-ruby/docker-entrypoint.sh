fb393290e49f6754e69f7e9d4850c41fe5058e85
Leo Zhu <leo18945@gmail.com>
2019-05-29 11:08:02 +0800

    delete redis-trib.rb from download but local reference

--- redis-ruby/docker-entrypoint.sh
+++ redis-ruby/docker-entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+#gem install redis \
+#  && wget https://raw.githubusercontent.com/antirez/redis/4.0/src/redis-trib.rb \
+#  && echo "yes" | ruby redis-trib.rb create --replicas 1 \
+#  10.0.0.2:7000 10.0.0.3:7001 10.0.0.4:7002 10.0.0.5:7003 10.0.0.6:7004 10.0.0.7:7005
+
 gem install redis \
-  && wget http://download.redis.io/redis-stable/src/redis-trib.rb \
   && echo "yes" | ruby redis-trib.rb create --replicas 1 \
   10.0.0.2:7000 10.0.0.3:7001 10.0.0.4:7002 10.0.0.5:7003 10.0.0.6:7004 10.0.0.7:7005
